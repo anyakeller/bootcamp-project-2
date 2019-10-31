6d6757211152e42fd22854109c174a38066675d0
Anya L Keller <kelleranyal@gmail.com>
2019-10-31 15:33:21 -0400

    Added more comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,14 @@ var db = require("../models");
 module.exports = function(app) {
     // OUR ROUTES
     // "/api/children" For the database of kids
+    // GET - gets kids
+    // POST - adds kid from form
     // "/api/parents" for the database of parents
+    // GET - gets parents
+    // POST - adds parents from form
     // "/IDKHELP" for the secure login for admins
+    // GET - for verifying login
+    // POST - for creating account
 
     // Get all examples
     app.get("/api/examples", function(req, res) {
