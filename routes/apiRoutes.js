bc72713ad2d79811f0aa89d79dbd2b4e4c019b35
Anya L Keller <kelleranyal@gmail.com>
2019-10-31 15:31:48 -0400

    Added comments for our api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,31 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+    // OUR ROUTES
+    // "/api/children" For the database of kids
+    // "/api/parents" for the database of parents
+    // "/IDKHELP" for the secure login for admins
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    // Get all examples
+    app.get("/api/examples", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.json(dbExamples);
+        });
     });
-  });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    // Create a new example
+    app.post("/api/examples", function(req, res) {
+        db.Example.create(req.body).then(function(dbExample) {
+            res.json(dbExample);
+        });
     });
-  });
+
+    // Delete an example by id
+    app.delete("/api/examples/:id", function(req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function(
+            dbExample
+        ) {
+            res.json(dbExample);
+        });
+    });
 };
