e4d7079b4e0d8ff8d2338c2c46d1a92ef39f3a80
Art Petrosyan <petrosyanart@gmail.com>
2019-11-02 11:54:24 -0400

    modulized routes and fixed get routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,35 +1,13 @@
 var db = require('../models');
 
 module.exports = function(app) {
-  //OUR ROUTES
-  // "/" Landing page with login/create account options
-  // "/home" page with week view and maybe a modal for the child profile
-  // "/createAdminAccount" self explainitory
-  // "/createParentAccount" self explainitory
-  // "/registerChild"
+ 
+  require('./childApiRoutes')(app);
+  require('./parentApiRoutes')(app);
+  require('./forms')(app);
+  require('./mainroutes')(app);
 
-  // Load index page
-  app.get('/', function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render('index', {
-        msg: 'Welcome!',
-        examples: dbExamples,
-      });
-    });
-  });
 
-  app.get('/genKids', function(req, res) {
-    res.render('testdbPostRoutes');
-  });
-
-  // Load example page and pass in an example by id
-  app.get('/example/:id', function(req, res) {
-    db.Example.findOne({where: {id: req.params.id}}).then(function(dbExample) {
-      res.render('example', {
-        example: dbExample,
-      });
-    });
-  });
 
   // Render 404 page for any unmatched routes
   app.get('*', function(req, res) {
